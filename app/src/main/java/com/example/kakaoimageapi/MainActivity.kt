6767514cf2825b4ed41126338bb127ba60ee1e2d
idkos <leegang31@gmail.com>
2024-08-08 15:37:15 +0900

    feat: 기본 화면 구성

--- app/src/main/java/com/example/kakaoimageapi/MainActivity.kt
+++ app/src/main/java/com/example/kakaoimageapi/MainActivity.kt
@@ -3,12 +3,49 @@ package com.example.kakaoimageapi
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.kakaoimageapi.ui.adapter.MainViewpagerAdapter
 import com.example.kakaoimageapi.databinding.ActivityMainBinding
+import com.example.kakaoimageapi.ui.bookmark.BookmarkFragment
+import com.example.kakaoimageapi.ui.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val list = listOf(
+            SearchFragment(),
+            BookmarkFragment()
+        )
+        val adapter = MainViewpagerAdapter(list, this)
+
+        binding.run {
+            mainViewpager.adapter = adapter
+
+            mainViewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    // ViewPager의 페이지가 변경될 때 BottomNavigationView의 선택 상태를 변경
+                    binding.mainBottomnavigation.menu.getItem(position).isChecked = true
+                }
+            })
+
+            mainBottomnavigation.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.search -> {
+                        mainViewpager.currentItem = 0
+                        true
+                    }
+                    R.id.folder -> {
+                        mainViewpager.currentItem = 1
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/kakaoimageapi/ui/search/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.example.kakaoimageapi.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kakaoimageapi.R
+import com.example.kakaoimageapi.databinding.FragmentSearchBinding
+
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+}
