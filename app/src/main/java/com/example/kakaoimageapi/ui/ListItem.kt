b33b2014393198212058d2c2f432b5970fea6096
idkos <leegang31@gmail.com>
2024-08-08 15:46:47 +0900

    feat: ListItem 생성

---
+++ app/src/main/java/com/example/kakaoimageapi/ui/ListItem.kt
@@ -0,0 +1,15 @@
+package com.example.androidtask.presentation
+
+sealed class ListItem {
+    data class ImageItem(
+        val uuid: String,
+        val thumbnailUrl: String,
+        val siteName: String,
+        val datetime: String,
+        val isBookmarked: Boolean = false
+    ) : ListItem()
+
+    data class LoadingItem(
+        val isLoading: Boolean
+    ) : ListItem()
+}
