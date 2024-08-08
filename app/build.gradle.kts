c2af463d50f929a2452b3a10b11fb483381504ab
idkos <leegang31@gmail.com>
2024-08-08 15:41:14 +0900

    add: 기본 설정 파일 추가 및 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -15,6 +17,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "KAKAO_API_KEY",
+            gradleLocalProperties(rootDir, providers).getProperty("KAKAO_API_KEY")
+        )
     }
 
     buildTypes {
@@ -36,6 +44,9 @@ android {
     buildFeatures{
         viewBinding = true
     }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -48,4 +59,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
 }
